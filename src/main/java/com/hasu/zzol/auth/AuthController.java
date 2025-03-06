9cb6ee3835ce11e80308e1797c7ac2500f8d7fd9
hayeonKim <iioo3356@gmail.com>
2025-03-07 01:09:37 +0900

    feat: 스웨거 설명 추가

--- src/main/java/com/hasu/zzol/auth/AuthController.java
+++ src/main/java/com/hasu/zzol/auth/AuthController.java
@@ -1,5 +1,8 @@
 package com.hasu.zzol.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
+@Tag(name="Auth", description = "로그인, 회원가입 등 비로그인 회원이 이용")
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
 
     @GetMapping("/sign-in/kakao")
-    @ResponseBody
+    @Operation(summary = "카카오 로그인", description = "프론트에서 전달한 인가 코드로 회원 여부 조회, 액세스 토큰 발급")
+    @Parameter(name = "code", description = "카카오 인가코드")
     public ResponseEntity<SignInResponseDto> kakaoLogin(HttpServletRequest request) {
         // 프론트에서 전달한 인가 코드로 카카오 accessToken 발급
         String code = request.getParameter("code");
@@ -22,7 +27,7 @@ public class AuthController {
     }
 
     @PostMapping("/sign-up/kakao")
-    @ResponseBody
+    @Operation(summary = "카카오 회원가입", description = "카카오 계정과 회원가입 페이지에서 작성한 정보로 회원 등록")
     public ResponseEntity<SignUpResponseDto> kakaoSignUp(@RequestBody SignUpRequestDto requestDto) {
         return ResponseEntity.ok(authService.kakaoSignUp(requestDto));
 
