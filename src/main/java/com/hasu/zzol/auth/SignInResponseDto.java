b0acaae154e43dab863ce9b14ef4352db8631882
hayeonKim <iioo3356@gmail.com>
2025-03-07 01:14:13 +0900

    fix: 회원가입 여부 not blank

--- src/main/java/com/hasu/zzol/auth/SignInResponseDto.java
+++ src/main/java/com/hasu/zzol/auth/SignInResponseDto.java
@@ -10,6 +10,7 @@ import lombok.Data;
 public class SignInResponseDto {
     @JsonProperty("isRegistered")
     @Schema(description = "회원가입을 한 회원인지 여부")
+    @NotBlank
     private boolean isRegistered; // 회원가입을 한 회원인지 여부
 
     @NotBlank
